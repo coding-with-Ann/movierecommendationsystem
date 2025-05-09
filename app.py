1b354ff5ae0a31b67e0245b7006546fcb300193a
coding-with-Ann <149531901+coding-with-Ann@users.noreply.github.com>
2025-05-09 20:15:50 +0530

    new commit

--- app.py
+++ app.py
@@ -43,21 +43,21 @@ if st.button("Recommend"):
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
-        st.header(names[0])
+        st.text(names[0])
         st.image(posters[0])
 
     with col2:
-        st.header(names[1])
+        st.text(names[1])
         st.image(posters[1])
 
     with col3:
-        st.header(names[2])
+        st.text(names[2])
         st.image(posters[2])
 
     with col4:
-        st.header(names[3])
+        st.text(names[3])
         st.image(posters[3])
 
     with col5:
-        st.header(names[4])
+        st.text(names[4])
         st.image(posters[4])
